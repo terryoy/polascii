8aa3a1eab29c460dd28ea18a241570541c292217
terryoy <terry.ouyang@gmail.com>
2014-04-04 01:41:49 +0800

    fix image title

--- pc/uploader.py
+++ pc/uploader.py
@@ -34,7 +34,7 @@ class PolasciiUploader:
         from_header = '<TITLE>Ascii arted image done using aalib</TITLE>'
         to_header = '<meta charset=utf-8><title>Ascii Image from Project Polascii - Supported by SZDIY</title><link rel=stylesheet href="/style/main.css">'
         from_body = '<BODY BGCOLOR="#000000" TEXT="#b2b2b2" LINK="#FFFFFF">'
-        to_body = '<BODY BGCOLOR="#000000" TEXT="#b2b2b2" LINK="#FFFFFF">\n<center><div class="head"><span style="font-size: 56px;font-weight: bold;font-color=#aaaaaa"><a style="text-decoration: none;color: white;" href="/index.html">IMAGE FROM POLASCII</a></span></div></center>'
+        to_body = '<BODY BGCOLOR="#000000" TEXT="#b2b2b2" LINK="#FFFFFF">\n<center><div class="head"><span style="font-size: 56px;font-weight: bold;font-color=#aaaaaa"><a style="text-decoration: none;color: white;" href="/index.html">POLASCII IMAGE</a></span></div></center>'
         from_footer = '</BODY>'
         to_footer = '<p>&nbsp;</p><center><footer><p>Project Polascii, created by <a href="http://github.com/terryoy">terryoy</a>, 2014. Supported by <a href="http://szdiy.org/">SZDIY community</a>.</p><p>Polascii项目由<a href="http://github.com/terryoy">terryoy</a>开发，<a href="http://szdiy.org/">SZDIY社区</a>支持。</p></footer></center></body>'
         return content.replace(from_header, to_header).replace(from_body, to_body).replace(from_footer, to_footer)
