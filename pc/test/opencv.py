a2955a29ed62ce6545ac1d1f5b2cf3d50770d561
terryoy <terry.ouyang@gmail.com>
2016-09-14 00:48:31 +0800

    fix with latest pip packages

--- pc/test/opencv.py
+++ pc/test/opencv.py
@@ -2,15 +2,20 @@ import cv2
 from PIL import Image
 import aalib
 from console import get_terminal_size
+import time
 
 h, w = get_terminal_size()
 screen = aalib.AsciiScreen(width=w, height=h)
 vc = cv2.VideoCapture(0)
 
+print('Ascii Screen initialized:', screen)
+print('CV2 initialized:', vc)
+
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
 else:
     rval = False
+    print('Camera is not opened', vc.isOpened())
 
 while rval:
     rval, frame = vc.read()
@@ -19,6 +24,8 @@ while rval:
     image = Image.fromstring('RGB', (width, height), frame.tostring())
     screen.put_image((0,0), image.convert('L').resize(screen.virtual_size))
     print(screen.render(contrast=70))
+
+    time.sleep(0.01)
 
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
--- pc/test/thermal.py
+++ pc/test/thermal.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-from escpos import *
+from escpos import printer
+#from escpos.printer import Usb
 
 # use "system_profiler SPUSBDataType" to check on Mac OSX
 #pp = printer.Usb(0x1a86, 0x7584, interface=0, in_ep=2, out_ep=2)
-pp = printer.Usb(0x6868, 0x0300, interface=0, in_ep=2, out_ep=2)
+pp = printer.Usb(0x6868, 0x0600, 0, 0x04, 0x03)
 
 # print Chinese
-pp.text(u'你好啊'.encode('gb2312'))
+pp.codepage = 'gb2312'
+pp.text(u'你好啊')
+pp.text('Hello, world!')
 pp.control('LF') # flush
-
+pp.cut('----')
 
